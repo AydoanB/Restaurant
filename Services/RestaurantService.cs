2f55388f880ee7b99d5488443a3abe9181a3c14c
AydoanB <balakchiev02@gmail.com>
2022-08-10 23:59:50 +0300

    Implement restaurant and receipt services

---
+++ Services/RestaurantService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ConsoleApp1;
+using WindowsFormsApp1.Models.Dishes;
+
+namespace RestaurantManager.Services
+{
+    public class RestaurantService
+    {
+        public Restaurant Restaurant => new Restaurant();
+
+        public int MakeOrder(int tableNumber, List<IDish> selectedDishes)
+        {
+            var table = Restaurant.Tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+
+            if (table == null)
+            {
+                table = new Table(tableNumber, selectedDishes);
+
+                Restaurant.Tables.Add(table);
+            }
+
+            table.AddDishes(selectedDishes);
+
+
+            return tableNumber;
+        }
+
+        public Table GetByTableNumber(int tableNumber)
+        {
+            return Restaurant.Tables.FirstOrDefault(x => x.TableNumber == tableNumber);
+        }
+    }
+}
